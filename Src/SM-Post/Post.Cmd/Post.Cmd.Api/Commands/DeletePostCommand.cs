a6064306976bef529d54ae17ea4133358e113786
Hesam Kashefi <hkashefi7@outlook.com>
2024-04-29 14:26:47 +0330

    added commands

---
+++ Src/SM-Post/Post.Cmd/Post.Cmd.Api/Commands/DeletePostCommand.cs
@@ -0,0 +1,9 @@
+using CQRS.Core.Commands;
+
+namespace Post.Cmd.Api.Commands
+{
+    public class DeletePostCommand : BaseCommand
+    {
+        public required string Username { get; set; }
+    }
+}

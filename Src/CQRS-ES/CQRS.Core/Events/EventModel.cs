7e633ed3410e58244b86c1bdf3cf178621250c05
Hesam Kashefi <hkashefi7@outlook.com>
2024-05-02 10:35:55 +0330

    added timestamp to event model

--- Src/CQRS-ES/CQRS.Core/Events/EventModel.cs
+++ Src/CQRS-ES/CQRS.Core/Events/EventModel.cs
@@ -8,6 +8,7 @@ namespace CQRS.Core.Events
         [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
         public string? Id { get; set; }
 
+        public DateTime TimeStamp { get; init; }
         public int Version { get; init; }
         public required Guid AggregateIdentified { get; init; }
         public required string AggregateType { get; init; }

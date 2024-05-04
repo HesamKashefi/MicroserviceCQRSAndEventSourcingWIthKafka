12e16146d57f2967eb9dd1d11fe56c56f4d7d876
Hesam Kashefi <hkashefi7@outlook.com>
2024-05-04 16:13:45 +0330

    updated the registration of the command dispatcher

--- Src/SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
+++ Src/SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
@@ -23,16 +23,23 @@ builder.Services.AddScoped<ICommandHandler, CommandHandler>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var commandHandler = builder.Services.BuildServiceProvider().GetRequiredService<ICommandHandler>();
-var dispatcher = new CommandDispatcher();
-dispatcher.RegisterHandler<NewPostCommand>(commandHandler.HandleAsync);
-dispatcher.RegisterHandler<EditMessageCommand>(commandHandler.HandleAsync);
-dispatcher.RegisterHandler<AddCommentCommand>(commandHandler.HandleAsync);
-dispatcher.RegisterHandler<EditCommentCommand>(commandHandler.HandleAsync);
-dispatcher.RegisterHandler<LikePostCommand>(commandHandler.HandleAsync);
-dispatcher.RegisterHandler<RemoveCommentCommand>(commandHandler.HandleAsync);
-dispatcher.RegisterHandler<DeletePostCommand>(commandHandler.HandleAsync);
-builder.Services.AddSingleton<ICommandDispatcher>(dispatcher);
+// register dispatcher handlers
+builder.Services.AddSingleton<ICommandDispatcher>(sp =>
+{
+    var commandHandler = sp.GetRequiredService<ICommandHandler>();
+
+    var dispatcher = new CommandDispatcher();
+
+    dispatcher.RegisterHandler<NewPostCommand>(commandHandler.HandleAsync);
+    dispatcher.RegisterHandler<EditMessageCommand>(commandHandler.HandleAsync);
+    dispatcher.RegisterHandler<AddCommentCommand>(commandHandler.HandleAsync);
+    dispatcher.RegisterHandler<EditCommentCommand>(commandHandler.HandleAsync);
+    dispatcher.RegisterHandler<LikePostCommand>(commandHandler.HandleAsync);
+    dispatcher.RegisterHandler<RemoveCommentCommand>(commandHandler.HandleAsync);
+    dispatcher.RegisterHandler<DeletePostCommand>(commandHandler.HandleAsync);
+
+    return dispatcher;
+});
 
 var app = builder.Build();
 

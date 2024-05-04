9eef40cbb99a3c600d9384160e66f8e4a555436c
Hesam Kashefi <hkashefi7@outlook.com>
2024-05-04 16:00:49 +0330

    added event producer

--- Src/SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
+++ Src/SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
@@ -1,3 +1,4 @@
+using Confluent.Kafka;
 using CQRS.Core.Domain;
 using CQRS.Core.Handlers;
 using CQRS.Core.Infrastructure;
@@ -13,6 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.Configure<MongoConfig>(builder.Configuration.GetSection(nameof(MongoConfig)));
+builder.Services.Configure<ProducerConfig>(builder.Configuration.GetSection(nameof(ProducerConfig)));
 builder.Services.AddScoped<IEventStoreRepository, EventStoreRepository>();
 builder.Services.AddScoped<IEventStore, EventStore>();
 builder.Services.AddScoped<IEventSourcingHandler<PostAggregate>, EventSourcingHandler>();
---
+++ Src/SM-Post/Post.Cmd/Post.Cmd.Infrastructure/Producers/EventProducer.cs
@@ -0,0 +1,32 @@
+﻿using Confluent.Kafka;
+using CQRS.Core.Events;
+using CQRS.Core.Producers;
+using Microsoft.Extensions.Options;
+using System.Text.Json;
+
+namespace Post.Cmd.Infrastructure.Producers;
+public class EventProducer(IOptions<ProducerConfig> options) : IEventProducer
+{
+    private readonly ProducerConfig _config = options.Value;
+
+    public async Task Produce<T>(string topic, T @event) where T : BaseEvent
+    {
+        var producer = new ProducerBuilder<string, string>(_config)
+            .SetKeySerializer(Serializers.Utf8)
+            .SetValueSerializer(Serializers.Utf8)
+            .Build();
+
+        var message = new Message<string, string>()
+        {
+            Key = Guid.NewGuid().ToString(),
+            Value = JsonSerializer.Serialize(@event, @event.GetType())
+        };
+
+        var deliveryResult = await producer.ProduceAsync(topic, message);
+
+        if (deliveryResult.Status != PersistenceStatus.NotPersisted)
+        {
+            throw new Exception($"Could not produce event: '{@event.GetType().Name}' to topic: '{topic}' due to reason: '{deliveryResult.Message}'");
+        }
+    }
+}

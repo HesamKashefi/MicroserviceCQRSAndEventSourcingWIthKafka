4423e3b6733f46d04a8f867c4882ad09c4f50aec
Hesam Kashefi <hkashefi7@outlook.com>
2024-05-06 13:16:33 +0330

    Entities added

---
+++ Src/SM-Post/Post.Query/Post.Query.Domain/Entities/CommentEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Post.Query.Domain.Entities;
+
+public class CommentEntity
+{
+    [Key]
+    public Guid CommentId { get; set; }
+    public required string Username { get; set; }
+    public required string Message { get; set; }
+    public DateTime CommentDate { get; set; }
+
+    public Guid PostId { get; private set; }
+
+    [JsonIgnore]
+    public virtual PostEntity? PostEntity { get; private set; }
+}
---
+++ Src/SM-Post/Post.Query/Post.Query.Domain/Entities/PostEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Post.Query.Domain.Entities;
+
+[Table("Posts")]
+public class PostEntity
+{
+    [Key]
+    public Guid PostId { get; set; }
+    public required string Author { get; set; }
+    public required string Message { get; set; }
+    public DateTime DatePosted { get; set; }
+    public int Likes { get; set; }
+    public virtual ICollection<CommentEntity> Comments { get; set; } = [];
+
+}

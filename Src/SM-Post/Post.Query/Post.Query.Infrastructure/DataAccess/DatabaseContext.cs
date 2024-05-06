3eb93f86b7d8144e1c8c724c4eb2f1081fe17efc
Hesam Kashefi <hkashefi7@outlook.com>
2024-05-06 13:44:05 +0330

    added db context

---
+++ Src/SM-Post/Post.Query/Post.Query.Infrastructure/DataAccess/DatabaseContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Post.Query.Domain.Entities;
+
+namespace Post.Query.Infrastructure.DataAccess;
+public class DatabaseContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<PostEntity> Posts { get; init; }
+    public DbSet<CommentEntity> Comments { get; init; }
+}
---
+++ Src/SM-Post/Post.Query/Post.Query.Infrastructure/DataAccess/DatabaseContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Post.Query.Infrastructure.DataAccess;
+
+public class DatabaseContextFactory(Action<DbContextOptionsBuilder> configureDbContext)
+{
+    private readonly Action<DbContextOptionsBuilder> _configureDbContext = configureDbContext;
+
+    public DatabaseContext CreateDbContext()
+    {
+        var optionsBuilder = new DbContextOptionsBuilder();
+        _configureDbContext(optionsBuilder);
+
+        return new DatabaseContext(optionsBuilder.Options);
+    }
+}

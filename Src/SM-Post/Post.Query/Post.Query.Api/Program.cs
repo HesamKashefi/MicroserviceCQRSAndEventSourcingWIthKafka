11b72127990ac2fb0dcc872ef7da4ac9ab9ac9ec
Alireza Nikravesh <a.z.nikravesh@gmail.com>
2024-05-11 18:44:30 +0330

    added extension for Post.Query.Api

--- Src/SM-Post/Post.Query/Post.Query.Api/Program.cs
+++ Src/SM-Post/Post.Query/Post.Query.Api/Program.cs
@@ -1,29 +1,9 @@
-using Confluent.Kafka;
-using CQRS.Core.Consumers;
-using Microsoft.EntityFrameworkCore;
-using Post.Query.Domain.Repositories;
-using Post.Query.Infrastructure.Consumers;
+using Post.Query.Api.Extensions.HostingExtensions;
 using Post.Query.Infrastructure.DataAccess;
-using Post.Query.Infrastructure.Handlers;
-using Post.Query.Infrastructure.Repostirories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<IPostRepository, PostRepository>();
-builder.Services.AddScoped<ICommentsRepository, CommentsRepostirory>();
-builder.Services.AddScoped<IEventHandler, Post.Query.Infrastructure.Handlers.EventHandler>();
-builder.Services.Configure<ConsumerConfig>(builder.Configuration.GetSection(nameof(ConsumerConfig)));
-builder.Services.AddScoped<IEventConsumer, EventConsumer>();
-
-builder.Services.AddHostedService<ConsumerHosterService>();
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-Action<DbContextOptionsBuilder> configure = (options) => options.UseLazyLoadingProxies().UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
-builder.Services.AddDbContext<DatabaseContext>(configure);
-builder.Services.AddSingleton(new DatabaseContextFactory(configure));
+builder.Services.AddServices(builder);
 
 var app = builder.Build();
 
